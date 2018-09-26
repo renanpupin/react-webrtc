b4e6d92c17b4b63deafbcbf043c7d8465e6adc5a
Renan Pupin <renan.pupin@gmail.com>
2018-09-26 15:38:03 -0300

    fix app js

--- app.js
+++ app.js
@@ -6,20 +6,20 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 let fs = require('fs');
 let open = require('open');
 let options = {
-  key: fs.readFileSync('./fake-keys/privatekey.pem'),
-  cert: fs.readFileSync('./fake-keys/certificate.pem')
+    key: fs.readFileSync('./fake-keys/privatekey.pem'),
+    cert: fs.readFileSync('./fake-keys/certificate.pem')
 };
-let serverPort = (process.env.PORT  || 5000);
+let serverPort = (process.env.PORT || 5000);
 let https = require('https');
 let http = require('http');
 
 let server;
 if (process.env.LOCAL) {
-  server = https.createServer(options, app);
+    server = https.createServer(options, app);
 } else {
-  server = http.createServer(app);
+    server = http.createServer(app);
 }
-let io = require('socket.io')(server, { pingTimeout: 30000 });
+let io = require('socket.io')(server, {pingTimeout: 30000});
 
 // app.get('/', function(req, res){
 //   console.log('get /');
@@ -28,71 +28,72 @@ let io = require('socket.io')(server, { pingTimeout: 30000 });
 
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-server.listen(serverPort, function(){
-  console.log('server up and running at %s port', serverPort);
-  if (process.env.LOCAL) {
-    open('https://localhost:' + serverPort)
-  }
+server.listen(serverPort, function () {
+    console.log('server up and running at %s port', serverPort);
+    if (process.env.LOCAL) {
+        open('https://localhost:' + serverPort)
+    }
 });
 
 
 //socket
 let roomList = {};
+
 function socketIdsInRoom(name) {
-  var socketIds = io.nsps['/'].adapter.rooms[name];
-  if (socketIds) {
-    var collection = [];
-    for (var key in socketIds) {
-      collection.push(key);
+    let socketIds = io.nsps['/'].adapter.rooms[name];
+    if (socketIds) {
+        let collection = [];
+        for (let key in socketIds) {
+            collection.push(key);
+        }
+        return collection;
+    } else {
+        return [];
     }
-    return collection;
-  } else {
-    return [];
-  }
 }
 
-io.on('connection', function(socket){
-  console.log('connection');
-  
-  socket.on('disconnect', function(){
-    console.log('disconnect');
-    if (socket.room) {
-      var room = socket.room;
-      io.to(room).emit('leave', socket.id);
-      socket.leave(room);
-    }
-  });
+io.on('connection', function (socket) {
+    console.log('connection');
 
-  // socket.on('leave', function(){
-  //   console.log('leave');
-  //   if (socket.room) {
-  //     var room = socket.room;
-  //     io.to(room).emit('leave', socket.id);
-  //     socket.leave(room);
-  //   }
-  // });
+    socket.on('disconnect', function () {
+        console.log('disconnect');
+        if (socket.room) {
+            let room = socket.room;
+            io.to(room).emit('leave', socket.id);
+            socket.leave(room);
+        }
+    });
 
-  socket.on('join', function(name, callback){
-    console.log('join', name);
-    var socketIds = socketIdsInRoom(name);
-    callback(socketIds);
-    socket.join(name);
-    socket.room = name;
-  });
+    // socket.on('leave', function(){
+    //   console.log('leave');
+    //   if (socket.room) {
+    //     var room = socket.room;
+    //     io.to(room).emit('leave', socket.id);
+    //     socket.leave(room);
+    //   }
+    // });
+
+    socket.on('join', function (name, callback) {
+        console.log('join', name);
+        let socketIds = socketIdsInRoom(name);
+        callback(socketIds);
+        socket.join(name);
+        socket.room = name;
+    });
 
 
-  socket.on('exchange', function(data){
-    console.log('exchange', data);
-    data.from = socket.id;
-    var to = io.sockets.connected[data.to];
-    to.emit('exchange', data);
-  });
+    socket.on('exchange', function (data) {
+        console.log('exchange', data);
+        data.from = socket.id;
+        let to = io.sockets.connected[data.to];
+        to.emit('exchange', data);
+    });
 
-  socket.on('synctimer', function(data){
-    console.log('synctimer', data);
-    io.to(data.room).emit('synctimer', data);
-  });
+    socket.on('synctimer', function (data) {
+        console.log('synctimer', data);
+        io.to(data.room).emit('synctimer', data);
+    });
 });
