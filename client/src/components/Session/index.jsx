bdbae16714247fa6e36a9f48e9334bcaf0e8b6d3
João Edezio Maiorchini da Rocha <joaomaiorchini@gmail.com>
2018-09-27 01:08:50 -0300

    add peers communication

--- client/src/components/Session/index.jsx
+++ client/src/components/Session/index.jsx
@@ -7,7 +7,6 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || 
 
 const configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
 
-const socket = socketIO();
 
 export default class Session extends Component {
     constructor(props) {
@@ -15,10 +14,11 @@ export default class Session extends Component {
         this.state = {
             selfVideoStream: null,
             connectedToSocket: false,
+            pcPeers: [],
             othersPeers: [],
         };
 
-        this.pc = null;
+        this.socket = socketIO();
     }
 
     logError(error) {
@@ -35,12 +35,101 @@ export default class Session extends Component {
         }, this.logError);
     }
 
+    createOffer(pc) {
+        pc.createOffer(function(desc) {
+            console.log('createOffer', desc);
+            pc.setLocalDescription(desc, function () {
+                console.log('setLocalDescription', pc.localDescription);
+                socket.emit('exchange', {'to': socketId, 'sdp': pc.localDescription });
+            }, logError);
+        }, logError);
+    }
+
+    createDataChannel(pc) {
+        if (pc.textDataChannel) {
+            return;
+        }
+        let dataChannel = pc.createDataChannel("text");
+
+        dataChannel.onerror = (error) => {
+            console.log("dataChannel.onerror", error);
+        };
+
+        dataChannel.onmessage = (event) => {
+            console.log("dataChannel.onmessage:", event.data);
+            // var content = document.getElementById('textRoomContent');
+            // content.innerHTML = content.innerHTML + '<p>' + socketId + ': ' + event.data + '</p>';
+        };
+
+        dataChannel.onopen = () => {
+            console.log('dataChannel.onopen');
+            // var textRoom = document.getElementById('textRoom');
+            // textRoom.style.display = "block";
+        };
+
+        dataChannel.onclose = () => {
+            console.log("dataChannel.onclose");
+        };
+
+        pc.textDataChannel = dataChannel;
+    }
+
+    createPC(socketId, isOffer) {
+        let pc = new RTCPeerConnection(configuration);
+        let pcPeers = this.state.pcPeers;
+        pcPeers[socketId] = pc;
+
+        pc.onicecandidate = (event) => {
+            console.log('onicecandidate', event);
+            if (event.candidate) {
+                this.socket.emit('exchange', {'to': socketId, 'candidate': event.candidate });
+            }
+        };
+
+
+        pc.onnegotiationneeded = () => {
+            console.log('onnegotiationneeded');
+            if(isOffer) {
+                this.createOffer(pc);
+            }
+        };
+
+        pc.oniceconnectionstatechange = (event) => {
+            console.log('oniceconnectionstatechange', event);
+            if(event.target.iceConnectionState === 'connected') {
+                this.createDataChannel(pc);
+            }
+        };
+        pc.onsignalingstatechange = (event) => {
+            console.log('onsignalingstatechange', event);
+        };
+
+        pc.onaddstream = (event) => {
+            console.log('onaddstream', event);
+            // var element = document.createElement('video');
+            let otherPeer = {
+                id: "remoteView" + socketId,
+                stream: event.stream,
+            };
+            this.setState({
+                othersPeers: [...this.state.othersPeers, otherPeer],
+            });
+            // element.id = "remoteView" + socketId;
+            // element.autoplay = 'autoplay';
+            // element.src = URL.createObjectURL(event.stream);
+            // remoteViewContainer.appendChild(element);
+        };
+        pc.addStream(this.state.selfVideoStream);
+        return pc;
+    }
+
+
     join() {
-        socket.emit('join', "SECOMPP2018", (socketIds) => {
+        this.socket.emit('join', "SECOMPP2018", (socketIds) => {
             console.log('join', socketIds);
             for (var i in socketIds) {
-                var socketId = socketIds[i];
-                // createPC(socketId, true);
+                let socketId = socketIds[i];
+                this.createPC(socketId, true);
             }
             this.setState({
                 connectedToSocket: true,
@@ -52,16 +141,16 @@ export default class Session extends Component {
     renderOtherPeers(otherPeer, index) {
         return (
             <Peer
-                id={"remoteView" + index}
+                id={otherPeer.id}
                 key={index}
-                videoSource={this.state.selfVideoStream ? URL.createObjectURL(this.state.selfVideoStream) : null}
+                videoSource={otherPeer.stream ? URL.createObjectURL(otherPeer.stream) : null}
             />
         );
     }
 
 
     render() {
-        const othersPeers = this.state.othersPeers.map((item, index) => this.renderOtherPeers(item, index))
+        const othersPeers = this.state.pcPeers.map((item, index) => this.renderOtherPeers(item, index));
         return (
             <div className={"rw-session-container"}>
                 <Peer
